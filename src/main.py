2e45a58a06855db8fc1e96dbb67785f647b9efad
Katya <katya.a.grekova@gmail.com>
2018-08-12 15:15:52 -0700

    added coin picture

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 
-class TestApp(App):
+import random
+
+class FlipACoinApp(App):
+    image = Image(source = 'CanadianToonieTails.png', size = (50, 50))
+
     def build(self):
-        return Button(text='Hello World')
+        self.image.bind(on_touch_down = self.callback)
+        boxLayout = BoxLayout(orientation = 'vertical')
+        boxLayout.add_widget(self.image)
+        return boxLayout
+ 
+    def callback(self, value, a):
+        head = random.choice([True, False])
+        if (head):
+            self.image.source = 'CanadianToonieHead.png'
+        else:
+            self.image.source = 'CanadianToonieTails.png'
 
-TestApp().run()
+if __name__ == '__main__':
+    FlipACoinApp().run()
